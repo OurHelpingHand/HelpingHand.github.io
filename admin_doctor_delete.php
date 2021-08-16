073b8b6b589397715324631514df0670154c61de
OurHelpingHand <89032116+OurHelpingHand@users.noreply.github.com>
2021-08-16 22:02:52 +0530

    Delete admin_doctor_delete.php

--- admin_doctor_delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$con = mysqli_connect('localhost','root','','registration');
-$id = $_GET['doc_id'];
-$reg= "delete from doctors where doc_id = '$id' ";
-$query = mysqli_query($con,$reg);
-header('location:admin_doctor.php');
-   
-?>
