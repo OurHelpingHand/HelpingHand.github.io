d35edc8654ec249a6a24ea040105acac580518a0
OurHelpingHand <89032116+OurHelpingHand@users.noreply.github.com>
2021-08-16 22:04:46 +0530

    Delete admin_user_delete.php

--- admin_user_delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$con = mysqli_connect('localhost','root','','registration');
-$id = $_GET['uid'];
-$reg= "delete from user_reg where uid = '$id' ";
-$query = mysqli_query($con,$reg);
-header('location:admin_user.php');
-   
-?>
