08c997d2fbe6e67b0ec8bc9d5eeafb13ddb43e82
OurHelpingHand <89032116+OurHelpingHand@users.noreply.github.com>
2021-08-16 22:03:35 +0530

    Delete admin_oxygen_delete.php

--- admin_oxygen_delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$con = mysqli_connect('localhost','root','','registration');
-$id = $_GET['oxy_id'];
-$reg= "delete from oxygen where oxy_id = '$id' ";
-$query = mysqli_query($con,$reg);
-header('location:admin_oxygen.php');
-   
-?>
